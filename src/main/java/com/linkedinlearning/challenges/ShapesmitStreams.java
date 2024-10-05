b8af2d71cd77fffd84752e2cb529197b60f507db
Sephora Bidiamba <112778042+goalHunter96@users.noreply.github.com>
2024-10-05 00:46:36 +0000

    Challenge: ShapemitStream, Schwierigkeitsgrad: Mittel, Notiz: Die Klassen Circle, Rectangle, Square, Triangle und Shape müssen noch übertragen werden !

---
+++ src/main/java/com/linkedinlearning/challenges/ShapesmitStreams.java
@@ -0,0 +1,27 @@
+package com.linkedinlearning.challenges;
+
+import java.util.ArrayList;
+
+/*
+    * Nutze die Stream API, um die Summe der Fläche der Shapes innerhalb eines Streams zu brechnen
+    * Der stream kann aus einer Collection oder mithilfe von Stream.of() erzeugt werden
+    * Nutze die speziellen Stream-Methode für primitive Datentypen wie mapToInt() für effizienteren Code
+    */
+public class ShapesmitStreams {
+/*
+Die Klassen Circle, Rectangle, Square, Triangle und Shape müssen noch übertragen werden !
+ * public static void main(String[] args) {
+ * ArrayList<Shape> shapes = new ArrayList<>();
+ * shapes.add(new Circle(4));
+ * shapes.add(new Rectangle(4,5));
+ * shapes.add(new Square(6));
+ * shapes.add(new Triangle(3,4,5));
+ * 
+ * double sum = shapes.stream()
+ * .mapToDouble(Shape::getArea)
+ * .sum();
+ * System.out.println(sum);
+ * }
+ */ 
+ 
+}
